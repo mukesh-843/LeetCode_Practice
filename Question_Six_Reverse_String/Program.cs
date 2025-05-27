e165795e5d1364f9bfd51c3e8e7806bde1f17a09
mukesh <gmukesh843@gmail.com>
2025-05-27 22:00:53 +0530

    reverse_string

---
+++ Question_Six_Reverse_String/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Solution {
+    public void ReverseString(char[] s) {
+        int left = 0;
+        int right = s.Length - 1;
+
+        while (left < right) {
+            // Swap the characters
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}
+
+class Program {
+    static void Main(string[] args) {
+        char[] input = { 'h', 'e', 'l', 'l', 'o' };
+
+        Console.WriteLine("Original: " + new string(input));
+
+        Solution solution = new Solution();
+        solution.ReverseString(input);
+
+        Console.WriteLine("Reversed: " + new string(input));
+    }
+}
