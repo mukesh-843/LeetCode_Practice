c7ab8d5684556938dc3f8cc81575cd2874dd6c85
mukesh <gmukesh843@gmail.com>
2025-05-28 21:53:13 +0530

    two sum II

---
+++ Question_Seven_two_Sum_II/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution {
+    public int[] TwoSum(int[] numbers, int target) {
+
+    Dictionary<int, int> dict = new Dictionary<int, int>();
+
+        for(int i = 0; i < numbers.Length; i++){
+            int diff_target = target - numbers[i];
+
+            if(dict.ContainsKey(diff_target)){
+                return new int[] {dict[diff_target]+1, i+1};
+            }
+
+            if(!dict.ContainsKey(numbers[i])){
+                dict[numbers[i]] = i;
+            }
+
+        }
+
+        return new int[0];
+    }
+}
