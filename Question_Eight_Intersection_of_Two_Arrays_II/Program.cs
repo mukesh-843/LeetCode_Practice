5f0b8db8f4f1cdb11fae937d051935ff9a849c09
mukesh <gmukesh843@gmail.com>
2025-05-29 23:29:28 +0530

    Question_Eight_Intersection_of_Two_Arrays_II

---
+++ Question_Eight_Intersection_of_Two_Arrays_II/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution {
+    public int[] Intersect(int[] nums1, int[] nums2) {
+        var counts = new Dictionary<int, int>();
+        var result = new List<int>();
+
+        
+        foreach (int num in nums1) {
+            if (counts.ContainsKey(num))
+                counts[num]++;
+            else
+                counts[num] = 1;
+        }
+
+       
+        foreach (int num in nums2) {
+            if (counts.ContainsKey(num) && counts[num] > 0) {
+                result.Add(num);
+                counts[num]--;
+            }
+        }
+
+        return result.ToArray();
+    }
+}
